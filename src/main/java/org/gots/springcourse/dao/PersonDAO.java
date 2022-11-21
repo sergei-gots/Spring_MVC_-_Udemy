5b87c35ba3e246ab92ed622cee9e6401ac3a06e9
Sergei Gots <Serge.Gots@gmail.com>
2022-11-21 22:15:33 +0300

    Lesson 32 "id int GENERATED BY DEFAULT AS IDENTITY" completed:)

--- src/main/java/org/gots/springcourse/dao/PersonDAO.java
+++ src/main/java/org/gots/springcourse/dao/PersonDAO.java
@@ -13,7 +13,6 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private final JdbcTemplate jdbcTemplate;
-
     @Autowired
     public PersonDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -21,25 +20,22 @@ public class PersonDAO {
     public List<Person> index() {
         return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
-
     public Person show(int id) {
         return jdbcTemplate.query("SELECT * FROM person WHERE id=?",
                 new Object[] {id} ,
                 new int[] { Types.INTEGER },
                 new BeanPropertyRowMapper<>(Person.class) ).stream().findAny().orElse(null);
     }
-
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO person VALUES (?,?,?,?)", person.getId(), person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO person(name, age, email) VALUES (?,?,?)",
+                person.getName(),
+                person.getAge(),
+                person.getEmail());
     }
-
-
-
     public void update(int id, Person updatedPerson) {
         jdbcTemplate.update("UPDATE person SET name=?, age=?, email=? WHERE id=?",
                 updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
     }
-
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
     }
